d89649abbe751eb935bebfefe0570617a36bd36a
Rohit Athithya <rohitathithya@gmail.com>
2025-12-31 13:11:13 -0700

    09.../04_adder.v >> solution for set adder circuit| Exams/m2014q4j -  2 solutions were explored with module instance arrays and for-loop unrolling instantiation; ignore changes in other files

--- 09_arithmetic_circuits/03_3_bit_binary_adder.v
+++ 09_arithmetic_circuits/03_3_bit_binary_adder.v
@@ -1,1 +1,58 @@
- 
+/* Problem statement: Adder3 | 3 bit binary adder
+* Now that you know how to build a full adder, make 3 instances of it
+* to create a 3-bit binary ripple-carry adder.
+* The adder adds two 3-bit numbers and a carry-in to produce a 3-bit sum and carry out.
+* To encourage you to actually instantiate full adders,
+* also output the carry-out from each full adder in the ripple-carry adder. cout[2] is the final carry-out from the last full adder, and is the carry-out you usually see.
+*/
+
+module fadd
+(
+    //output(s)
+    output sum, cout,
+
+    //input(s)
+    input a, b, cin
+);
+
+    assign sum = a ^ b ^ cin;
+    assign cout = a&b | b&cin | a&cin;
+
+endmodule
+
+module top_module
+(
+    //output(s)
+    output [2:0] cout,
+    output [2:0] sum,
+
+    //input(s)
+    input [2:0] a, b,
+    input cin
+);
+    fadd fadd_bit0 (
+        .sum(sum[0]),
+        .cout(cout[0]),
+
+        .a(a[0]),
+        .b(b[0]),
+        .cin(cin)
+    );
+    fadd fadd_bit1 (
+        .sum(sum[1]),
+        .cout(cout[1]),
+
+        .a(a[1]),
+        .b(b[1]),
+        .cin(cout[0])
+    );
+    fadd fadd_bit2 (
+        .sum(sum[2]),
+        .cout(cout[2]),
+
+        .a(a[2]),
+        .b(b[2]),
+        .cin(cout[1])
+    );
+
+endmodule
--- 09_arithmetic_circuits/04_adder.v
+++ 09_arithmetic_circuits/04_adder.v
@@ -1,1 +1,68 @@
- 
+/* Problem statement: adder circuit |
+* Implement the given circuit
+* the image has this;
+* 4 full adders :
+* inputs are 4 bit x and y vectors, no cin => cin = 0
+* 4 adders generate 4bit sum,
+* finals adder's cout is sum[4]
+*/
+
+module fadd
+(
+    //output(s)
+    output sum, cout,
+
+    //input(s)
+    input a, b, cin
+);
+
+    assign sum = a ^ b ^ cin;
+    assign cout = a&b | b&cin | a&cin;
+
+endmodule
+
+
+module top_module
+(
+    //output(s)
+    output [4:0] sum,
+
+    //input(s)
+    input [3:0] x, y
+);
+
+    //SOLUTION 2: Instantiation based implementation
+    wire [4:0] temp_cout; // can be reg also
+    assign temp_cout[0] = '0; //if not provided, then temp_cout[0] becomes ambiguous,
+                              // synthesis tool may or may not assume it to be 0
+    assign sum[4] = temp_cout[4];
+
+    fadd fadd_instances [3:0] (
+        .sum(sum[3:0]),
+        .cout(temp_cout[4:1]),
+        .a(x[3:0]),
+        .b(y[3:0]),
+        .cin(temp_cout[3:0])
+    );
+
+
+    // //SOLUTION 1: For-loop based implementation
+    // genvar i;
+    // reg [4:0] temp_cout;
+
+    // generate
+    //     assign temp_cout[0] = 1'b0;
+    //     for (i = 0; i<4; i=i+1) begin: circuitry
+    //         fadd fadd_tmp (
+    //             .sum(sum[i]),
+    //             .cout(temp_cout[i+1]),
+
+    //             .a(x[i]),
+    //             .b(y[i]),
+    //             .cin(temp_cout[i])
+    //         );
+    //     end
+    //     assign sum[4] = temp_cout[4];
+    // endgenerate
+
+endmodule
