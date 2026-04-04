43cb60d914e5fa961c26e05890b3353831be14e9
Rohit Athithya <rohitathithya@gmail.com>
2026-04-04 13:19:51 -0700

    */06_100_bit_binary_adder.v >> solution for adder100 or exams/ece241_2041_q1c problem

--- 09_arithmetic_circuits/06_100_bit_binary_adder.v
+++ 09_arithmetic_circuits/06_100_bit_binary_adder.v
@@ -1,1 +1,24 @@
- 
+/*
+* Problem statement: Create a 100-bit binary adder.
+* The adder adds two 100-bit numbers and a carry-in to produce a 100-bit sum and carry out.
+* 
+*/
+// the problem setter expects a single line solution.
+// other approaches are ignored because they have already been discussed with lesser number of bits,
+// here the simplest/expected solution has been provided.
+
+module top_module
+#(
+    parameter NUM_BITS = 100
+)
+(
+    //output(s)
+    output cout,
+    output [NUM_BITS-1:0] sum,
+
+    //inputs(s)
+    input [NUM_BITS-1:0] a, b,
+    input cin
+);
+    assign {cout, sum} = a + b + cin;
+endmodule
