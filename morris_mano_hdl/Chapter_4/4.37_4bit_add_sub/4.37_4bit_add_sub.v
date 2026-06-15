725c77b6d433c0ca207d5d742bd94957638a222e
Rohit Athithya <rohitathithya@gmail.com>
2026-06-14 18:47:34 -0700

    untrack create_files.py; hdl solution to problem4.37 - verified the output

---
+++ morris_mano_hdl/Chapter_4/4.37_4bit_add_sub/4.37_4bit_add_sub.v
@@ -0,0 +1,69 @@
+module half_adder( 
+    output sum, carry_out, 
+    input inp_1, inp_2
+); 
+    xor (sum, inp_1, inp_2);
+    and (carry_out, inp_1, inp_2);
+endmodule
+
+module full_adder ( 
+    output sum, carry_out, 
+    input inp_1, inp_2, inp_3
+); 
+
+    wire S1, C1, C2;
+    
+    half_adder HA1 (S1, C1, inp_1, inp_2);
+    half_adder HA2 (sum, C2, S1, inp_3);
+    or G1 (carry_out, C2, C1);
+
+endmodule
+
+
+module adder_4bit ( 
+    output [3: 0] sum, 
+    output C4,
+
+    input [3: 0] A, 
+    input [3:0] B, 
+    input C0
+);
+
+    wire C1, C2, C3; 
+
+    full_adder 
+        FA0 (sum[0], C1, A[0], B[0], C0),
+        FA1 (sum[1], C2, A[1], B[1], C1),
+        FA2 (sum[2], C3, A[2], B[2], C2),
+        FA3 (sum[3], C4, A[3], B[3], C3);
+    
+
+endmodule
+
+
+
+module four_bit_add_sub ( //unsigned 4 bit adder_subtractor circuit - (no overflow bit)
+    output [3: 0] sum, 
+    output C4,
+
+    input [3:0] A, 
+    input [3:0] B,
+    input mode // mode =0 => adder, mode=1 => subtractor
+);
+
+    wire [3:0] b_gated;
+    xor
+        g_xor_3 (b_gated[3], B[3], mode), //mode=0 => pass, mode=1 => toggle
+        g_xor_2 (b_gated[2], B[2], mode), 
+        g_xor_1 (b_gated[1], B[1], mode), 
+        g_xor_0 (b_gated[0], B[0], mode); 
+
+    adder_4bit 
+        F4addsub (
+            .sum(sum), .C4(C4),
+            .A(A), .B(b_gated), .C0(mode)
+        );
+
+
+
+endmodule
