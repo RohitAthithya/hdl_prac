1de18ae1bfb5a11f28f52011c35854932108a927
Rohit Athithya <rohitathithya@gmail.com>
2026-07-20 20:09:23 -0700

    4.23_2_to_4_decoder: solution + verification. active low enable

--- morris_mano_hdl/Chapter_4/4.23_2_to_4_decoder/4.23_2_to_4_decoder.v
+++ morris_mano_hdl/Chapter_4/4.23_2_to_4_decoder/4.23_2_to_4_decoder.v
@@ -1,8 +1,25 @@
 module decoder_2_to_4(
-    output [3:0] out,
-    input [1:0] in
-);
+    output [3:0] out,   //4 outputs
 
+    input [1:0] in,     //2 inputs
+    input enable_       //active low enable
+);
+    wire [3:0] out_;    //inverted outputs
+    wire [1:0] in_;     //inverted inputs
+    wire en_;           // enable'
+    nand
+        (en_, enable_, enable_),
+        (in_[1], in[1], in[1]),
+        (in_[0], in[0], in[0]),
+        (out[3], out_[3], out_[3]),
+        (out[2], out_[2], out_[2]),
+        (out[1], out_[1], out_[1]),
+        (out[0], out_[0], out_[0]),
+
+        (out_[0], en_, in_[1], in_[0]),
+        (out_[1], en_, in_[1], in[0]),
+        (out_[2], en_, in[1], in_[0]),
+        (out_[3], en_, in[1], in[0]);
     
 
 endmodule
--- morris_mano_hdl/Chapter_4/4.23_2_to_4_decoder/tb_4.23_2_to_4_decoder.v
+++ morris_mano_hdl/Chapter_4/4.23_2_to_4_decoder/tb_4.23_2_to_4_decoder.v
@@ -1,1 +1,38 @@
- 
+`timescale 1ps/1ps
+
+
+module tb_2to4_decoder;
+
+    reg [1:0] inp;
+    reg en;
+    wire [3:0] out;
+
+    initial begin
+        $dumpfile("2to4_decoder.vcd");
+        $dumpvars(0, tb_2to4_decoder);
+    end
+
+    decoder_2_to_4 DUT (
+        .out(out),
+        .in(inp),
+        .enable_(en)
+    );
+
+    initial begin
+        $monitor("enable=%b : input=%2b => output=%4b",en, inp, out );
+        en = 1; //circuit doesn't output anything
+        inp = 2'b00;
+        repeat (3) begin
+            #1  inp = inp + 1;
+        end
+
+        #1 en = 0;  //circuit becomes active - throws the decoder outptus
+        inp = 2'b00;
+        repeat (3) begin
+            #1  inp = inp + 1;
+        end
+
+    end
+
+
+endmodule
