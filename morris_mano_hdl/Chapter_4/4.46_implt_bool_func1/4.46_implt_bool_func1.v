1c8d78d427ee6d98a8fd985f73c8ca86380e2383
Rohit Athithya <rohitathithya@gmail.com>
2026-07-21 19:31:51 -0700

    prob. 4.46: boolean function impelemntation using 16:1 mux; --4.47 misnomer folder

--- morris_mano_hdl/Chapter_4/4.46_implt_bool_func1/4.46_implt_bool_func1.v
+++ morris_mano_hdl/Chapter_4/4.46_implt_bool_func1/4.46_implt_bool_func1.v
@@ -1,1 +1,40 @@
- 
+module mux_16to1(
+    output out,
+
+    input [16-1:0] in,
+    input [$clog2(16)-1:0] sel
+);
+    assign out = in[sel];
+
+endmodule
+
+module bool_expr_4_46_b(
+    output F,
+    input A, 
+    input B, 
+    input C, 
+    input D
+);
+    mux_16to1 mux1 (
+        .out(F),
+        .in({16'b1111_0111_1011_1011}),
+        .sel({A,B,C,D})
+    );
+endmodule
+
+
+
+module bool_expr_4_46_a(
+    output F,
+    input A, 
+    input B, 
+    input C, 
+    input D
+);
+    mux_16to1 mux1 (
+        .out(F),
+        .in({16'b0100_0101_0010_0101}),
+        .sel({A,B,C,D})
+    );
+
+endmodule
