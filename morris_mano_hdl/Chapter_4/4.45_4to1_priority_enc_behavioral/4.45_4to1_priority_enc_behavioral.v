eccf3b0eed9181271e407b2dd78d7af74bbe5070
Rohit Athithya <rohitathithya@gmail.com>
2026-07-21 18:41:51 -0700

    4:2 priority encoder: prob. 4.49

--- morris_mano_hdl/Chapter_4/4.45_4to1_priority_enc_behavioral/4.45_4to1_priority_enc_behavioral.v
+++ morris_mano_hdl/Chapter_4/4.45_4to1_priority_enc_behavioral/4.45_4to1_priority_enc_behavioral.v
@@ -1,1 +1,27 @@
- 
+module priority_encoder_4to2(
+    output reg x, y, V,
+
+    input [3:0] D
+);
+
+    always @(*) begin
+        V = 1'b1;
+        if (D[3] == 1) begin
+            {x, y} = 2'b11;
+        end
+        else if (D[3] == 0 & D[2] == 1) begin
+            {x, y} = 2'b10;
+        end
+        else if (D[3] == 0 & D[2] ==0 & D[1] == 1) begin
+            {x, y} = 2'b01;
+        end
+        else if (D == 4'b0001) begin
+            {x, y} = 2'b00;
+        end
+        else begin
+            {x, y} = 2'b00;
+            V = 1'b0;
+        end
+    end
+
+endmodule
--- morris_mano_hdl/Chapter_4/4.45_4to1_priority_enc_behavioral/tb_4.45_4to1_priority_enc_behavioral.v
+++ morris_mano_hdl/Chapter_4/4.45_4to1_priority_enc_behavioral/tb_4.45_4to1_priority_enc_behavioral.v
@@ -1,1 +1,27 @@
- 
+`timescale 1ps/1ps
+
+module tb_priority_encoder_4to2;
+    wire x, y, V;
+    reg [3:0] combined;
+
+    initial begin
+        $monitor(" inp=%4b | x=%b, y=%b, V=%b", combined, x, y, V);
+    end
+
+    initial begin
+        $dumpfile("prio_enc_4to2.vcd");
+        $dumpvars(0, tb_priority_encoder_4to2);
+    end
+
+    priority_encoder_4to2 DUT (
+        .x(x), .y(y), .V(V),
+        .D(combined)
+    );
+
+    initial begin
+        combined = 4'b0000;
+        repeat (15) begin
+            #1 combined = combined + 1;
+        end
+    end
+endmodule
