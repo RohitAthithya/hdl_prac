6c9f9e86d99560ec1a25799d27330b2749822cd5
Rohit Athithya <rohitathithya@gmail.com>
2026-07-21 18:58:15 -0700

    4:2 priority encoder improvements - based on if else statements and using casex statement

--- morris_mano_hdl/Chapter_4/4.45_4to1_priority_enc_behavioral/4.45_4to1_priority_enc_behavioral.v
+++ morris_mano_hdl/Chapter_4/4.45_4to1_priority_enc_behavioral/4.45_4to1_priority_enc_behavioral.v
@@ -4,24 +4,92 @@ module priority_encoder_4to2(
     input [3:0] D
 );
 
+    // always @(*) begin
+    //     V = ~(D == 4'b0000);
+
+    //     if (D[3]) begin
+    //         {x, y} = 2'b11;
+    //     end
+    //     else if (D[2]) begin
+    //         {x, y} = 2'b10;
+    //     end
+    //     else if (D[1]) begin
+    //         {x, y} = 2'b01;
+    //     end
+    //     else begin
+    //         {x, y} = 'b00;
+    //     end
+    // end
+
+    
     always @(*) begin
         V = 1'b1;
-        if (D[3] == 1) begin
-            {x, y} = 2'b11;
-        end
-        else if (D[3] == 0 & D[2] == 1) begin
-            {x, y} = 2'b10;
-        end
-        else if (D[3] == 0 & D[2] ==0 & D[1] == 1) begin
-            {x, y} = 2'b01;
-        end
-        else if (D == 4'b0001) begin
-            {x, y} = 2'b00;
-        end
-        else begin
-            {x, y} = 2'b00;
-            V = 1'b0;
-        end
+        casez (D)
+            4'b1???: {x,y} = 2'b11; 
+            4'b01??: {x,y} = 2'b10; 
+            4'b001?: {x,y} = 2'b01; 
+            4'b0001: {x,y} = 2'b00; 
+            default: begin
+                {x,y} = 2'b00;
+                V = 1'b0; 
+            end
+        endcase
     end
+
+    //non synthesizeable code - as we are using casex - use casez instead
+    // always @(*) begin
+    //     V = 1'b1;
+    //     casex (D)
+    //         4'b1xxx: {x,y} = 2'b11; 
+    //         4'b01xx: {x,y} = 2'b10; 
+    //         4'b001x: {x,y} = 2'b01; 
+    //         4'b0001: {x,y} = 2'b00; 
+    //         default: begin
+    //             {x,y} = 2'b00;
+    //             V = 1'b0; 
+    //         end
+    //     endcase
+    // end
+
+    //type 1
+    // always @(*) begin
+    //     V = 1'b1;
+    //     if (D == 4'b0000)
+    //         V = 1'b0;
+
+    //     if (D[3] == 1) begin
+    //         {x, y} = 2'b11;
+    //     end
+    //     else if (D[2] == 1) begin
+    //         {x, y} = 2'b10;
+    //     end
+    //     else if (D[1] == 1) begin
+    //         {x, y} = 2'b01;
+    //     end
+    //     else begin
+    //         {x, y} = 2'b00;
+    //     end
+    // end
+
+    //type 2
+    // always @(*) begin
+    //     V = 1'b1;
+    //     if (D[3] == 1) begin
+    //         {x, y} = 2'b11;
+    //     end
+    //     else if (D[3] == 0 & D[2] == 1) begin
+    //         {x, y} = 2'b10;
+    //     end
+    //     else if (D[3] == 0 & D[2] ==0 & D[1] == 1) begin
+    //         {x, y} = 2'b01;
+    //     end
+    //     else if (D == 4'b0001) begin
+    //         {x, y} = 2'b00;
+    //     end
+    //     else begin
+    //         {x, y} = 2'b00;
+    //         V = 1'b0;
+    //     end
+    // end
 
 endmodule
